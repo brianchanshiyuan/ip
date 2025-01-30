9f2b6a0be62df10b9612d40b05a8d5292058cf97
brianchanshiyuan <e1155337@u.nus.edu>
2025-01-31 00:35:27 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/nova.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class nova {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Nova");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+    }
+}
