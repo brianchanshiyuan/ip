9e23f800ffee5cd4c03d6b0c2e11407ac682355f
brianchanshiyuan <e1155337@u.nus.edu>
2025-01-31 00:40:21 +0800

    Level 1. Echo

--- src/main/java/nova.java
+++ src/main/java/nova.java
@@ -2,15 +2,32 @@ import java.util.Scanner;
 
 public class nova {
     public static void main(String[] args) {
+        // Greeting message
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Nova");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
+        Scanner scanner = new Scanner(System.in);
+
+        String input;
+        while (true) {
+            input = scanner.nextLine().trim();
+
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
 
         System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
-
     }
 }
+
