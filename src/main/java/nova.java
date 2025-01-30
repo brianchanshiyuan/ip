ed4ce107ec1684e98797388c5028e4d77b2666fc
brianchanshiyuan <e1155337@u.nus.edu>
2025-01-31 00:43:21 +0800

    Level 2. Add, List

--- src/main/java/nova.java
+++ src/main/java/nova.java
@@ -2,32 +2,36 @@ import java.util.Scanner;
 
 public class nova {
     public static void main(String[] args) {
-        // Greeting message
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Nova");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         String input;
-        while (true) {
-            input = scanner.nextLine().trim();
-
-            if (input.equalsIgnoreCase("bye")) {
-                break;
+        while (!(input = scanner.nextLine()).equals("bye")) {
+            if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
             }
-
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
         }
 
         scanner.close();
-
         System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 }
-
